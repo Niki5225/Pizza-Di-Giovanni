18223d6e27aab5dd7410913eb30e49fd6786e319
Niki5225 <nikolayradkov97.im@gmail.com>
2023-08-07 15:05:27 +0300

    added requirements and custom admin

--- pizza_app2/pizza_app2/accounts/admin.py
+++ pizza_app2/pizza_app2/accounts/admin.py
@@ -1,11 +1,58 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
+from django.contrib.auth import admin as auth_admin, get_user_model
 
-# Register your models here.
+from pizza_app2.accounts.forms import EditUserForm, RegisterUserForm
 
 UserModel = get_user_model()
 
 
 @admin.register(UserModel)
-class UserModelAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(auth_admin.UserAdmin):
+    form = EditUserForm
+    add_form = RegisterUserForm
+
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'username',
+                    'password',
+                ),
+            }),
+        (
+            'Personal info',
+            {
+                'fields': (
+                    'first_name',
+                    'last_name',
+                    'email',
+                    'profile_picture'
+                ),
+            },
+        ),
+        (
+            'Permissions',
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                    'groups',
+                    'user_permissions',
+                ),
+            },
+        ),
+        (
+            'Important dates',
+            {
+                'fields': (
+                    'last_login',
+                    'date_joined',
+                ),
+            },
+        ),
+    )
+
+    def get_form(self, request, obj=None, **kwargs):
+        return super().get_form(request, obj, **kwargs)
